d564d212a298230cb7e9c889a85b3e2021fe0e4c
ikanu <iamkartiknarwal@gmail.com>
2016-10-14 03:24:02 +0530

    Added the synaptics mouse scripts and modified the i3 config file

---
+++ scripts/mouse.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Profile" 0
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Constant Deceleration" 6
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Adaptive Deceleration" 6
+
+
